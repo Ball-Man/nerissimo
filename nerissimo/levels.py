61b5f5cf54ae34aef4cb163435dee7fdc2a0de83
Francesco Mistri <francesco.mistri98@gmail.com>
2026-01-31 23:11:20 +0100

    Add nerissimo level

--- nerissimo/levels.py
+++ nerissimo/levels.py
@@ -1,8 +1,10 @@
 import desper
 import sdl2
+from sdl2 import sdlttf as ttf
 
 from . import graphics
 from . import game
+from .log import logger
 
 
 def base_square_level_transformer(handle: desper.WorldHandle,
@@ -40,7 +42,44 @@ def base_square_level_transformer(handle: desper.WorldHandle,
     world.create_entity(game.NextOnWin())
 
 
+def base_nerissimo_level_transformer(handle: desper.WorldHandle,
+                                     world: desper.World):
+    """Instantiate game world basics (common to all platforms)."""
+    world.add_processor(desper.CoroutineProcessor())
+    world.add_processor(graphics.TimeProcessor())
+
+    # Setup screen rendering
+    world.create_entity(graphics.ScreenSurfaceHandler())
+    screen_surface, screen_surface_array = graphics.prepare_surface_array_components(
+            sdl2.SDL_CreateRGBSurfaceWithFormat(0, graphics.BONNET_WIDTH,
+                                                graphics.BONNET_HEIGHT, 1,
+                                                sdl2.SDL_PIXELFORMAT_RGB332))
+    world.create_entity(
+        graphics.ScreenSurface(), screen_surface, screen_surface_array)
+
+    nerissimo = graphics.render_text('fonts/exepixelperfect', 'NERISSIMO')
+
+    world.create_entity(
+        desper.Transform2D(position=(2, 2)),
+        *graphics.prepare_surface_array_components(nerissimo),
+        game.Velocity(0, 0),
+        game.UserControlled(),
+        graphics.EnsureClipped())
+
+    world.create_entity(
+        desper.Transform2D(position=(20, 2)),
+        *graphics.prepare_surface_array_components(nerissimo))
+
+    world.add_processor(graphics.TimeProcessor(1 / 30))
+    world.add_processor(game.VelocityProcessor())
+    world.add_processor(game.WinConditionProcessor(screen_surface_array), 1000)
+    world.add_processor(graphics.ClipTransformsProcessors((0, 0, graphics.BONNET_HEIGHT,
+                                                           graphics.BONNET_WIDTH)))
+
+    world.create_entity(game.NextOnWin())
+
+
 transformer_list = [
     ('square', base_square_level_transformer),
-    ('square2', base_square_level_transformer)
+    ('square2', base_nerissimo_level_transformer),
 ]
