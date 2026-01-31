83d48035db55254189361750de65b1b82f1ff7b5
Francesco Mistri <francesco.mistri98@gmail.com>
2026-01-31 23:38:02 +0100

    Add scripts necessary for freezing the build

---
+++ desktop_main.py
@@ -0,0 +1,5 @@
+from nerissimo import start_game
+
+
+if __name__ == '__main__':
+    start_game(on_bonnet=False, window_scale=6)
---
+++ freeze.py
@@ -0,0 +1,26 @@
+from cx_Freeze import setup, Executable
+import sys
+
+TARGET = 'nerissimo'
+EXCLUDE = []
+PACKAGES = []
+INCLUDE_FILES = ['resources']
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+# Optimize = 1 keeps docstrings, unfortunately it is required by numpy
+# which would crash without them (unknown reason
+build_options = {'packages': PACKAGES, 'excludes': EXCLUDE, 'optimize': 2,
+                 'include_files': INCLUDE_FILES}
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+
+executables = [
+    Executable('desktop_main.py', base=base, target_name=TARGET),
+]
+
+setup(name='Nerissimo',
+      version='1.0',
+      description='',
+      options={'build_exe': build_options},
+      executables=executables)
