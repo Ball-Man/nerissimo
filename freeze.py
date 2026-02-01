d4a5f8fb8f6b9fb2afa0f698ccbca035f1410da8
Francesco Mistri <francesco.mistri98@gmail.com>
2026-02-01 14:45:05 -0800

    Update freeze script with ico

--- freeze.py
+++ freeze.py
@@ -10,13 +10,13 @@ INCLUDE_FILES = ['resources']
 # fine tuning.
 # Optimize = 1 keeps docstrings, unfortunately it is required by numpy
 # which would crash without them (unknown reason
-build_options = {'packages': PACKAGES, 'excludes': EXCLUDE, 'optimize': 1,
+build_options = {'packages': PACKAGES, 'excludes': EXCLUDE, 'optimize': 2,
                  'include_files': INCLUDE_FILES, 'include_msvcr': True}
 
 base = 'gui' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('desktop_main.py', base=base, target_name=TARGET),
+    Executable('desktop_main.py', base=base, target_name=TARGET, icon='icon.ico'),
 ]
 
 setup(name='Nerissimo',
